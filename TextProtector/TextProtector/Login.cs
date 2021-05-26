a359f7f487d406ee0340da23160ab3fb052a86fa
Simp <61764480+YungSamzy@users.noreply.github.com>
2021-05-26 20:24:38 -0200

    Initial Release

---
+++ TextProtector/TextProtector/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextProtector
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == AES.Decrypt("eyJpdiI6Ik1pVzFLYmpUOWRiYnFxZGVzNWwyOVE9PSIsInZhbHVlIjoiSGljdytwM0k4SFFKK1JBcnRjZ1JtQT09IiwibWFjIjoiYTQxMjBkMmUzYTU3OTkyYmRjY2ZiYjQ5OWE2ODdjZmNhZmJiNTY3NDkyYzgyM2I3N2ExMTI3NzMyOTM5OTBmNyJ9"))
+            {
+                MessageBox.Show("Logged In Successfully!", "Text Protector", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Main f1 = new Main();
+                this.Hide();
+                f1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Password!", "Text Protector", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ TextProtector/TextProtector/Main.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextProtector
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) //Result
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Encrypt
+        {
+            try
+            {
+                string file = textBox1.Text.Replace("\"", "");
+                string text = File.ReadAllText($@"{file}");
+                string encrypted = AES.Encrypt(text);
+                richTextBox1.Text = encrypted;
+            }
+            catch
+            {
+                MessageBox.Show("Error Encrypting Text! Make sure you have the right file!", "Text Protector", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) //File
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Decrypt
+        {
+            try
+            {
+                string file = textBox1.Text.Replace("\"", "");
+                string text = File.ReadAllText($@"{file}");
+                string decrypted = AES.Decrypt(text);
+                richTextBox1.Text = decrypted;
+            }
+            catch
+            {
+                MessageBox.Show("Error Decrypting Text! Make sure you have the right file!", "Text Protector", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(richTextBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(textBox1.Text, richTextBox1.Text);
+            MessageBox.Show("Saved!", "Text Protector", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Text Files|*.txt|All Files|*.*";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                textBox1.Text = selectedFileName;
+            }
+        }
+
+        private void textBox1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (files != null && files.Any())
+                textBox1.Text = files.First();
+        }
+    }
+}
